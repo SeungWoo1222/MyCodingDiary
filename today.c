f461c352d47f560c82eb86fa6c7835cc981c7175
Bemay <ilginam24@gamil.com>
2023-05-10 00:45:42 +0900

    2023.05.09
    오랜만에 공부다운 공부를 한 느낌!

--- today.c
+++ today.c
@@ -1,66 +1,114 @@
+#include <time.h>
 #include <stdio.h>
 
 int main(void)
 {	
-	////버스를 탄다고 가정. 학생/일반인으로 구분 (일반인:20세)
-	//int age = 25;
-	////if (조건) {...} else {...}
-	//if (age >= 20)
-	//{
-	//	printf("일반인 입니다\n");
-	//}
-	//else
-	//{
-	//	printf("학생입니다\n");
-	//}
-
-	//if / else if / else
-	/*int age = 8;
-	if (age >= 8 && age <= 13)
+//---------------------------여기서부터 2023.05.09 시작---------------------------
+	// &&그리고 ||혹은
+	/*int a = 10;
+	int b = 10;
+	int c = 12;
+	int d = 13;
+	if (a == b || c == d)
 	{
-		printf("초등학생입니다\n");
-	}
-	else if (age >= 14 && age <= 16)
-	{
-		printf("중학생입니다\n");
-	}
-	else if (age >= 17 && age <= 19)
-	{
-		printf("고등학생입니다\n");
+		printf("a 와 b, 혹은 c 와 d 의 값이 같습니다\n");
 	}
 	else
 	{
-		printf("학생이아닌가봐요\n");
+		printf("값이 서로 다르네요\n");
 	}*/
 
-	//break /continue
-	// 1번부터 30번까지 있는 반에서 1번에서 5번까지 조별 발표를 합니다.
-	/*for (int i = 1; i <= 30; i++)
+	// random #include <time.h> 추가
+	
+	//srand(time(NULL)); // 난수 초기화
+	//난수 초기화를 안하면 계속해서 같은 수가 반복해서 나옴
+	//ex)0 2 1 3 또 돌려도 0 2 1 3 이런식으로
+	//int num = rand() % 3; // 0 ~ 2
+	//int num = rand() % 3 + 1; // 1 ~ 3
+
+
+
+	//가위 0 바위 1 보 2
+	//srand(time(NULL));
+	//int i = rand() % 3; // 0 ~ 2 반환
+	/*if (i == 0)
 	{
-		if (i >= 6)
-		{
-			printf("나머지 학생은 집에가세요\n");
-			break; //이 조건을 만족 할 때 탈출.
-		}
-		printf("%d 번 학생은 조별 발표 준비를 하세요\n", i);
+		printf("가위\n");
+	}
+	else if (i == 1)
+	{
+		printf("바위\n");
+	}
+	else if (i == 2)
+	{
+		printf("보\n");
+	}
+	else
+	{
+		printf("몰라유\n");
 	}*/
 
-	//1번부터 30번까지 있는 반에서 7번 학생은 아파서 결석
-	//7번을 제외하고 6번부터 10번까지 조별발표를 하세요
-	//for (int i = 1; i <= 30; i++)
+	//srand(time(NULL));
+	//int i = rand() % 3; // 0 ~ 2 반환
+	//switch (i)
 	//{
-	//	if (i >= 6 && i <= 10)
-	//	{
-	//		if (i == 7)
-	//		{
-	//			printf("%d번 학생은 결석입니다", i);
-	//			continue; // 이 조건에 들어왔을 때 continue를 만나면 이후 문장을 수행하지않고 처음 동작으로 돌아감.
-	//		}
-	//		printf("%d번 학생은 조별 발표 준비를 하세요", i);
-	//	}
-	//	
+	//case 0:printf("가위\n"); break; // break를 안하면 아래 케이스와 디폴트 모두 출력.
+	//case 1:printf("가위\n"); break;
+	//case 2:printf("가위\n"); break;
+	//default:printf("몰라유\n"); break;
 	//}
 
+	/*int age = 8;
+	switch (age)
+	{
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13:printf("초등학생입니다\n"); break;
+	case 14:
+	case 15:
+	case 16:printf("중학생입니다\n"); break;
+	case 17:
+	case 18:
+	case 19:printf("고등학생입니다\n"); break;
+	default : printf("학생이 아닌가봐요\n"); break;
+	}*/
+
+
+	//Up and Down
+	srand(time(NULL));
+	int num = rand() % 100 + 1; // 1~ 100 사이의 숫자
+	printf("숫자: %d\n", num);
+	int answer = 0;
+	int chance = 5;
+	while (chance > 0)
+	{
+		printf("남은 기회 : %d\n", chance--);
+		printf("숫자를 입력하세요 : \n");
+		scanf_s("%d", &answer);
+		if (answer == num)
+		{
+			printf("정답입니다! \n");
+		}
+		else if (answer > num)
+		{
+			printf("Down! \n");
+		}
+		else if (answer < num)
+		{
+			printf("Up! \n");
+		}
+		else
+		{
+			printf("오류\n");
+		}
+	}
+	if (chance == 0)
+	{
+		printf("기회 끝!\n");
+	}
 
 
 	return 0;
--- today.py
+++ today.py
@@ -1,31 +1,82 @@
-# N ~ 2N-1
-#    *
-#   ***
-#  *****
-# *******
-#*********
-#*******
-#   *****
-#    ***
-#     *    
-
-N = int(input()) #5
+#어제꺼해결!
+N = int(input())
 c = 1
 d = 1
 e = 2
 for i in range(2*N):
-    f = N-i-1 # 6-6-1
-    g = i-N-1
-    c = 2*i-1 #11
-    if c > 2*N-1: 
+    f = N-i
+    g = i-N
+    c = 2*i-1 #9
+    if c > 2*N-1:
         d = 2*(i-e)-1 
-        print(" "*g,"*"*d)
+        print(" "*g+"*"*d)
         e += 2
     else:
-        print(" "*f,"*"*c)
-        
-# n = int(input())
-# for i in range(1, n):
-#     print(' '*(n-i) + '*'*(2*i-1))
-# for i in range(n, 0, -1):
-#     print(' '*(n-i) + '*'*(2*i-1))
+        print(" "*f+"*"*c)
+
+
+#백준 문제1)
+
+# Mississipi => ?
+# zZa => Z
+# z => Z
+# baaa => A
+
+
+# n = "Mississipi"
+# m = dict(i, n)
+# # for i in n:
+# #     x = n.count(i)
+# #     print(x)
+# print(m)
+
+# 딕셔너리로 넣으면은 {숫자, 문자} 빈도수 큰 숫자 = 문자가 뜨고 그 문자를 출력.
+
+words = "Canddy"
+unique_words = list(set(words)) # 입력받은 문자열에서 중복값을 제거 candy
+
+cnt_list = []
+for x in unique_words : #candy
+    cnt = words.count(x) #c1 a1 n1 d2 y1
+    cnt_list.append(cnt)  # count 숫자를 리스트에 append [1,1,1,2,1]
+
+if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+    print('?')
+else :
+    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치) [3]
+    print(unique_words[max_index])
+
+
+#백준 문제2)
+
+test_num= 5
+score_num = 9
+score = 100, 99, 98, 97, 96, 95, 94, 93, 91
+
+# #각 케이스마다 한줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째자리까지 출력한다
+
+aver = sum(score) / len(score)
+passer = []
+for i in score:
+    if i > aver:
+        passer.append(i)
+    
+
+percent = len(passer) / len(score) * 100
+print("{:.3f}%".format(percent))
+
+# 패서/전체 len
+
+
+# 백준 문제로 만들기
+
+n = int(input())
+score = list(map(int, input().split()))
+aver = sum(score[1:]) / score[0]
+passer = []
+for i in score[1:]:
+    if i > aver:
+        passer.append(i)
+    
+percent = len(passer) / score[0] * 100
+print("{:.3f}%".format(percent))
